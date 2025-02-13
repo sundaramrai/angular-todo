48fe0287d034e4ec3b9f4b1e7515e04df36462f0
Sundaram Rai <sundaram.rai@hommevillegroup.com>
2025-02-13 10:59:47 +0530

    update environment api url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 // src/environments/environment.ts
 export const environment = {
   production: false,
-  apiUrl: "https://my-todo-backend-zcj9.onrender.com/api"
+  apiUrl: 'http://localhost:3000/api'
 };
